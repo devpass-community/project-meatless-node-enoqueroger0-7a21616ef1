efba7ed25a4d24558460d79ec3d4d9afb1f276de
Enoque Rogerio <enoquerogerio15mti25@gmail.com>
2023-10-06 21:50:41 +0100

    create delivery price management

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -12,9 +12,9 @@ const addOrder = async(order) => {
     const Orders_query = `INSERT INTO Orders (restaurant_id, subtotal, freight, total_price, user_id, status) VALUES ('${restaurant_id}', '${subtotal}', '${freight}', '${user_id}', '${status}')`;
     const order_id = await insertQuery(Orders_query);
 
-    return await product_id_list.array.forEach(product_id => {
+    return product_id_list.forEach(async (product_id) => {
         const query = `INSERT INTO Order_Products (order_id, product_id) VALUES ('${order_id}', '${product_id}')`
-        insertQuery(query)
+        await insertQuery(query)
     });
 };
 
